1d7f67dde5b2d0212b0e9f008e4c91f94755b7af
Paul CORNEC <cornec@et.esiea.fr>
2023-05-04 22:51:30 +0200

    #1 Create information display

---
+++ ERASMUS Management/ERASMUS Management/InformationDisplay.Designer.cs
@@ -0,0 +1,204 @@
+﻿
+namespace ERASMUS_Management
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtFullName = new System.Windows.Forms.TextBox();
+            this.txtYearOfBirth = new System.Windows.Forms.TextBox();
+            this.btnChooseFile = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtCity = new System.Windows.Forms.TextBox();
+            this.txtFaculty = new System.Windows.Forms.TextBox();
+            this.txtRole = new System.Windows.Forms.TextBox();
+            this.txtRoleSpecificAttribute = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Full Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 107);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Year of birth:";
+            // 
+            // txtFullName
+            // 
+            this.txtFullName.Location = new System.Drawing.Point(214, 64);
+            this.txtFullName.Name = "txtFullName";
+            this.txtFullName.Size = new System.Drawing.Size(231, 22);
+            this.txtFullName.TabIndex = 2;
+            this.txtFullName.TextChanged += new System.EventHandler(this.txtFullName_TextChanged);
+            // 
+            // txtYearOfBirth
+            // 
+            this.txtYearOfBirth.Location = new System.Drawing.Point(214, 104);
+            this.txtYearOfBirth.Name = "txtYearOfBirth";
+            this.txtYearOfBirth.Size = new System.Drawing.Size(231, 22);
+            this.txtYearOfBirth.TabIndex = 3;
+            this.txtYearOfBirth.TextChanged += new System.EventHandler(this.txtYearOfBirth_TextChanged);
+            // 
+            // btnChooseFile
+            // 
+            this.btnChooseFile.Location = new System.Drawing.Point(12, 12);
+            this.btnChooseFile.Name = "btnChooseFile";
+            this.btnChooseFile.Size = new System.Drawing.Size(141, 28);
+            this.btnChooseFile.TabIndex = 4;
+            this.btnChooseFile.Text = "Choose file...";
+            this.btnChooseFile.UseVisualStyleBackColor = true;
+            this.btnChooseFile.Click += new System.EventHandler(this.btnChooseFile_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 146);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "City:";
+            // 
+            // txtCity
+            // 
+            this.txtCity.Location = new System.Drawing.Point(214, 143);
+            this.txtCity.Name = "txtCity";
+            this.txtCity.Size = new System.Drawing.Size(231, 22);
+            this.txtCity.TabIndex = 6;
+            this.txtCity.TextChanged += new System.EventHandler(this.txtCity_TextChanged);
+            // 
+            // txtFaculty
+            // 
+            this.txtFaculty.Location = new System.Drawing.Point(214, 184);
+            this.txtFaculty.Name = "txtFaculty";
+            this.txtFaculty.Size = new System.Drawing.Size(231, 22);
+            this.txtFaculty.TabIndex = 7;
+            this.txtFaculty.TextChanged += new System.EventHandler(this.txtFaculty_TextChanged);
+            // 
+            // txtRole
+            // 
+            this.txtRole.Location = new System.Drawing.Point(214, 227);
+            this.txtRole.Name = "txtRole";
+            this.txtRole.Size = new System.Drawing.Size(231, 22);
+            this.txtRole.TabIndex = 8;
+            this.txtRole.TextChanged += new System.EventHandler(this.txtRole_TextChanged);
+            // 
+            // txtRoleSpecificAttribute
+            // 
+            this.txtRoleSpecificAttribute.Location = new System.Drawing.Point(214, 270);
+            this.txtRoleSpecificAttribute.Name = "txtRoleSpecificAttribute";
+            this.txtRoleSpecificAttribute.Size = new System.Drawing.Size(231, 22);
+            this.txtRoleSpecificAttribute.TabIndex = 9;
+            this.txtRoleSpecificAttribute.TextChanged += new System.EventHandler(this.txtRoleSpecificAttribute_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 187);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 17);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Faculty:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 230);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 17);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Role:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 273);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(151, 17);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "[role specific attribute]:";
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(490, 317);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtRoleSpecificAttribute);
+            this.Controls.Add(this.txtRole);
+            this.Controls.Add(this.txtFaculty);
+            this.Controls.Add(this.txtCity);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnChooseFile);
+            this.Controls.Add(this.txtYearOfBirth);
+            this.Controls.Add(this.txtFullName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Login";
+            this.ShowIcon = false;
+            this.Text = "User data display";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtFullName;
+        private System.Windows.Forms.TextBox txtYearOfBirth;
+        private System.Windows.Forms.Button btnChooseFile;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtCity;
+        private System.Windows.Forms.TextBox txtFaculty;
+        private System.Windows.Forms.TextBox txtRole;
+        private System.Windows.Forms.TextBox txtRoleSpecificAttribute;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ ERASMUS Management/ERASMUS Management/InformationDisplay.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ERASMUS_Management
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void txtRoleSpecificAttribute_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtRole_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtFaculty_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtCity_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtFullName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnChooseFile_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtYearOfBirth_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
